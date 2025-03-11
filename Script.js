6dfcb5e0600a7bb20073599aab3a6a7622e9f12f
Srikanth Reddy <srikanthreddy0218@gmail.com>
2025-03-11 23:32:48 +0530

    Added file

---
+++ Script.js
@@ -0,0 +1,158 @@
+// 1st question
+
+var x1 = 10;
+console.log(x1);   //10
+let x2 = 10;
+console.log(x2);   //10
+const x3 =10;
+console.log(x3);   //10
+// 2nd question
+
+total-ampount                   //invaild variable: should not use hyphen in middle of the variables
+// 2nd place                      //invalid variable: should not use numbers infront of variables
+_UserID                       //valid variable     
+$discount                    //valid variable
+var                         //invalid variable: beacuse we should not use  reserved keywords as var name
+myvariable                 //valid variable
+
+// 3rd question
+let x4="srikanth"
+console.log(x4);                 //srikanth 
+console.log(typeof(x4));        //string
+
+let x5 = 7;
+console.log(x5);                //7
+console.log(typeof(x5));       //number
+
+let v=true;
+console.log(v);                 //true
+console.log(typeof(v));        //boolean
+
+let a;
+console.log(a);               //undefined
+console.log(typeof(a));       //undefined
+
+let nul=null;
+console.log(nul);                     //null
+console.log(typeof(nul));            //null
+
+// 4th question
+
+let c="42"
+console.log(a);                        //42
+console.log(typeof(a));               //string
+
+let d=42;                    
+console.log(b);                      //42
+console.log(typeof(b));             //number
+
+let e=true;
+console.log(e);                    //true
+console.log(typeof(e));            //boolean
+
+let f=false;
+console.log(f);                    //undefined
+console.log(typeof(f));           //undefined
+
+let fruits=["apple","banana"]
+console.log(fruits);              //fruits
+console.log(typeof(fruits));      //object
+
+let h=null;
+console.log(h);                   //null
+console.log(typeof(h));           //null
+
+// 5th question
+
+let i="Hello World"            
+console.log(i);                  //Hello world
+console.log(typeof(i));          //string
+
+let j=42;
+console.log(j);                 //42
+console.log(typeof(j));        //number
+
+let k=true;
+console.log(k);               //true
+console.log(typeof(k));       //boolean
+
+let m=[1,2,3]
+console.log(m);              //[1,2,3]
+console.log(typeof(m));     //object
+
+let n;
+console.log(n);            //undefined
+console.log(typeof(n));   //undefined
+
+// 6th question
+
+let first_name="srikanth reddy"
+let last_name=" bisu"
+console.log(first_name + last_name);
+
+// 7th question
+
+let marks=35;
+console.log(marks+2);       //37
+console.log(marks-5);       //32
+console.log(marks*2);       //64
+console.log(marks/2);       //32
+
+// 8th question
+
+console.log("Hello,JavaScript!");
+console.warn("Hello,JavaScript!");
+console.error("Hello,JavaScript!");
+console.info("Hello,JavaScript!");
+
+// 9th question
+// Srikanth Reddy
+/*chintu chinnodu*/
+
+// 10th question
+let num  =10;
+console.log(num);        //10
+ num +=5;               
+ console.log(num);        //15
+ num  *=2;
+ console.log(num);        //30
+ num  -=7;
+ console.log(num);        //23
+ num  /=3;
+ console.log(num);        //7.666667
+ num  %=4;
+console.log(num);         //3.66667
+
+// 11th question
+let x=5;
+console.log(x);          //5
+let y=x++;
+console.log(y);          //5
+let p=5;
+console.log(p);          //5
+let q=++p;
+console.log(q);          //6
+
+// 12th question
+let r=10;
+let s=20;
+
+console.log(r==s);         //false
+console.log(r===s);        //false
+console.log(r!=s);         //true
+console.log(r!==s);        //true
+console.log(r>s);          //false
+console.log(r<s);          //true
+console.log(r>=s);         //false
+console.log(r<=s);         //true
+let t="30";
+let u="40";
+console.log(r==s);         //false
+console.log(r===s);        //false
+console.log(r!=s);         //true
+console.log(r!==s);        //true
+console.log(r>s);          //false
+console.log(r<s);          //true
+console.log(r>=s);         //false
+console.log(r<=s);         //true
+   
